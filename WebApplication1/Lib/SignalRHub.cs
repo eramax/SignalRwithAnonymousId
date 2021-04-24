952aa5ddb6e1c1377469173d96a76d772019c9a3
Ahmed Essam <eng.eramax@gmail.com>
2021-04-24 09:09:17 +0200

    ok

--- WebApplication1/Lib/SignalRHub.cs
+++ WebApplication1/Lib/SignalRHub.cs
@@ -1,9 +1,13 @@
 using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
 
 namespace WebApplication1.Lib
 {
     public class SignalRHub : Hub
     {
-        
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
     }
 }
--- WebApplication1/Models/Product.cs
+++ WebApplication1/Models/Product.cs
@@ -1,7 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace WebApplication1.Models
 {
     public class Product
     {
+        [Key]
         public int Id { get; set; }
         public string Name { get; set; }
         public int Stock { get; set; }
